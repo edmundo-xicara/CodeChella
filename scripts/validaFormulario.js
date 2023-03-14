2613fc80e7fd98514ab5eae6be8e1b155900e4ab
Edmundo <edmundopires@outlook.com>
2023-03-14 15:22:11 -0300

    validação completa do formulário

--- scripts/validaFormulario.js
+++ scripts/validaFormulario.js
@@ -1,24 +1,29 @@
 const campoNome = document.querySelector("#nome-completo");
 const campoEmail = document.querySelector("#email");
 const campoCPF = document.querySelector("#cpf");
+const campoSetor = document.querySelector("#setor");
 const campoDataNascimento = document.querySelector("#data-nascimento");
+const campoTipoIngresso = document.querySelector("#tipo-ingresso");
 
-campoNome.setCustomValidity("Por favor, informe seu nome completo.");
-campoEmail.setCustomValidity("Por favor, digite seu email.");
-campoCPF.setCustomValidity("Por favor, informe seu CPF.");
-campoDataNascimento.setCustomValidity("Por favor, informe sua data de nascimento.");
+campoNome.setCustomValidity("Por favor, informe seu nome completo");
+campoEmail.setCustomValidity("Por favor, digite seu email");
+campoSetor.setCustomValidity("Por favor, escolha o setor que deseja ir");
+campoDataNascimento.setCustomValidity("Por favor, informe sua data de nascimento");
+campoTipoIngresso.setCustomValidity("Por favor, escolha o tipo de ingresso que deseja");
 
 formataCPF()
 
-campoNome.addEventListener("blur", () => validaNome());
-campoCPF.addEventListener("blur", () => validaCPF());
-campoEmail.addEventListener("blur", () => validaEmail());
-campoDataNascimento.addEventListener("blur", () => validaDataNascimento());
+campoNome.addEventListener("blur", () => {if(!validaNome()) mostraMsgErro(campoNome, "Este campo é obrigatório*");});
+campoEmail.addEventListener("blur", () => {if(!validaEmail()) mostraMsgErro(campoEmail, "O email digitado é inválido*");});
+campoCPF.addEventListener("blur", () => {if(!validaCPF()) mostraMsgErro(campoCPF, "O CPF informado não existe*");});
+campoSetor.addEventListener("blur", () => {if(!validaSetor()) mostraMsgErro(campoSetor, "É preciso escolher algum setor*");});
+campoDataNascimento.addEventListener("blur", () => {if(!validaDataNascimento()) {mostraMsgErro(campoDataNascimento, "Você deve ter pelo menos 10 anos de idade*");}});
+campoTipoIngresso.addEventListener("blur", () => {if(!validaTipoIngresso()) mostraMsgErro(campoTipoIngresso, "Escolha o tipo de ingresso desejado*");});
 
-campos = [campoNome, campoCPF, campoEmail, campoDataNascimento];
-
+let campos = [campoNome, campoEmail, campoCPF, campoDataNascimento];
+let camposSelect = [campoSetor, campoTipoIngresso]
 campos.forEach(campo => campo.addEventListener("keydown", () => resetaMsgValidacao(campo)));
-
+camposSelect.forEach(campo => campo.addEventListener("click", () => resetaMsgValidacao(campo)));
 
 function validaNome() {
     let nome = campoNome.value;
@@ -47,7 +52,7 @@ function validaEmail() {
             return true;
     }
 
-    campoEmail.setCustomValidity("O email digitado é inválido.");
+    campoEmail.setCustomValidity("O email digitado é inválido");
     return false;
 }
 
@@ -65,7 +70,7 @@ function validaCPF() {
     let cpf = campoCPF.value.replace(/[.-]/g, "");
 
     if(!cpf) {
-        return false;
+        return true;
     }
 
     if(cpf == "00000000000" || 
@@ -78,7 +83,7 @@ function validaCPF() {
         cpf == "77777777777" || 
         cpf == "88888888888" || 
         cpf == "99999999999") {
-            campoCPF.setCustomValidity("O CPF informado é inválido.");
+            campoCPF.setCustomValidity("O CPF informado é inválido");
             return false;
         }
 
@@ -90,7 +95,7 @@ function validaCPF() {
     if(resto > 9) resto = 0;
 
     if(resto != parseInt(cpf.charAt(9))) {
-        campoCPF.setCustomValidity("O CPF informado é inválido.");
+        campoCPF.setCustomValidity("O CPF informado é inválido");
         return false;
     }
 
@@ -102,7 +107,16 @@ function validaCPF() {
     if(resto > 9) resto = 0;
 
     if(resto != parseInt(cpf.charAt(10))) {
-        campoCPF.setCustomValidity("O CPF informado é inválido.");
+        campoCPF.setCustomValidity("O CPF informado é inválido");
+        return false;
+    }
+
+    return true;
+}
+
+function validaSetor() {
+    if(!validaLista(campoSetor)) {
+        campoSetor.setCustomValidity("Por favor, escolha o setor que deseja ir");
         return false;
     }
 
@@ -119,11 +133,34 @@ function validaDataNascimento() {
     if(diferenca < 10) {
         campoDataNascimento.setCustomValidity("Você precisa ter pelo menos 10 anos de idade para comprar um ingresso");
         return false;
+    } else if(diferenca < 16) {
+        campoDataNascimento.parentNode.querySelector(".campo__msg-erro").innerHTML = "Você só poderá entrar acompanhado de um responsável*";
     }
 
     return true;
 }
 
+function validaTipoIngresso() {
+    if(!validaLista(campoTipoIngresso)) {
+        campoTipoIngresso.setCustomValidity("Por favor, escolha o tipo de ingresso que deseja");
+        return false;
+    }
+
+    return true;
+}
+
+function validaLista(campo) {
+    return !campo.value == '';
+}
+
+function mostraMsgErro(campo, msg) {
+    campo.style.backgroundColor = "#de5151";
+    campo.setAttribute("placeholder", '');
+    campo.parentNode.querySelector(".campo__msg-erro").innerHTML = msg;
+}
+
 function resetaMsgValidacao(campo) {
     campo.setCustomValidity('');
+    campo.style.backgroundColor = '';
+    campo.parentNode.querySelector(".campo__msg-erro").innerHTML = '';
 }
