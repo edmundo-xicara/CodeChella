9f811cfd812481c26e8ad65ef65387c8bc57da6e
Edmundo <edmundopires@outlook.com>
2023-03-16 12:08:40 -0300

    muda favicon de acordo com o tema do usuário

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,26 @@ window.onload = () => {
         botaoTrocaTema.checked = true;
         mudaTemaVerao();
     } 
+
+    mudaFavicon();
 }
 
+function mudaFavicon() {
+    const favicon = document.getElementById("favicon");
 
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if(mediaQuery.matches) {
+        favicon.setAttribute("href", "assets/img/favicon_branco.png");
+    } else {
+        favicon.setAttribute("href", "assets/img/favicon.png");
+    }
+
+    mediaQuery.addEventListener("change", (evento) => {
+        if(evento.matches) {
+            favicon.setAttribute("href", "assets/img/favicon_branco.png");
+        } else {
+            favicon.setAttribute("href", "assets/img/favicon.png");
+        }
+    });
+}
